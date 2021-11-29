e2d57c6a37d9a55a657522f48bf7d285d0633168
Ana Carolina Gonçalves de Oliveira <acoliveira@fitec.org.br>
2021-11-28 21:41:18 -0300

    entites and enuns

---
+++ Store.Domain/Entities/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿namespace Store.Domain.Entities
+{
+    class OrderItem : Entity
+    {
+        public OrderItem(Product product, decimal price, int quantity)
+        {
+            Product = product;
+            Price = Product != null ? product.Price : 0;
+            Quantity = quantity;
+        }
+
+        public Product Product { get; private set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; private set; }
+
+        public decimal Total() => Price * Quantity;
+    }
+}
---
+++ Store.Domain/Entities/Product.cs
@@ -0,0 +1,24 @@
+﻿namespace Store.Domain.Entities
+{
+    class Product : Entity
+    {
+        public Product(string title, decimal price, bool active)
+        {
+            Title = title;
+            Price = price;
+            Active = active;
+        }
+
+        public string Title { get; private set; }
+        public decimal Price { get; private set; }
+        public bool Active { get; private set; }
+
+        //método para fazer a troca dos valores
+        //public void ChangeInfo(string title, decimal price, bool active)
+        //{
+        //    Title = title;
+        //    Price = price;
+        //    Active = active;
+        //}
+    }
+}
