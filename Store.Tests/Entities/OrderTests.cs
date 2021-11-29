aacbf0d0ee4415c1b7f6c03109a7df152b4a67c6
Ana Carolina Gonçalves de Oliveira <acoliveira@fitec.org.br>
2021-11-28 23:36:44 -0300

    created tests methods

---
+++ Store.Tests/Entities/OrderTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Store.Tests.Entities
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderGenerateGuid8Caracters() { Assert.Fail(); }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderPaymentStatusWatingPayment() { Assert.Fail(); }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderPaymentStatusWatingDelivery() { Assert.Fail(); }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderPaymentStatusCanceled() { Assert.Fail(); }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderProductNull() { Assert.Fail(); }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderQuantityIsZero() { Assert.Fail(); }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderTotalIs50() { Assert.Fail(); }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderExpiredDiscountTotalIs60() { Assert.Fail(); }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderInvalidDiscountTotalIs60() { Assert.Fail(); }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderDiscountIs10TotalIs50() { Assert.Fail(); }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderDeliveryFeeIs10TotalIs60() { Assert.Fail(); }
+
+        [TestMethod]
+        [TestCategory("Domain")]
+        public void OrderInvalidCustomer() { Assert.Fail(); }
+    }
+}
